a1a40983640c7ed3b412e4f876ef067da464f6d0
Kevin <stradtkt22@gmail.com>
2019-02-24 16:12:46 -0500

    Added components

---
+++ frontend/src/components/leads/Leads.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Leads extends Component {
+    render() {
+        return (
+            <div className="Leads">
+                <h1>Leads</h1>
+            </div>
+        );
+    }
+}
+
+export default Leads;
