e416ec4d1cdd55a6b027ad00b9c409532e4957fc
Kevin <stradtkt22@gmail.com>
2019-02-24 17:24:31 -0500

    Added delete

--- frontend/src/actions/leads.js
+++ frontend/src/actions/leads.js
@@ -1,11 +1,21 @@
 import axios from 'axios';
-import { GET_LEADS } from "./types";
+import { GET_LEADS, DELETE_LEAD } from "./types";
 
 export const getLeads = () => dispatch => {
     axios.get('/api/leads/')
         .then(res => {
             dispatch({
                 type: GET_LEADS,
+                payload: res.data
+            });
+        }).catch(error => console.log(error));
+};
+
+export const deleteLead = (id) => dispatch => {
+    axios.delete(`/api/leads/${id}/`)
+        .then(res => {
+            dispatch({
+                type: DELETE_LEAD,
                 payload: res.data
             });
         }).catch(error => console.log(error));
--- frontend/src/reducers/leads.js
+++ frontend/src/reducers/leads.js
@@ -1,4 +1,4 @@
-import { GET_LEADS } from '../actions/types';
+import { GET_LEADS, DELETE_LEAD } from '../actions/types';
 
 const initialState = {
     leads: []
@@ -11,7 +11,13 @@ export default function(state = initialState, action) {
                 ...state,
                 leads: action.payload
             };
+        case DELETE_LEAD:
+            return {
+                ...state,
+                leads: state.leads.filter(lead => lead.is !== action.payload)
+            };
         default:
             return state;
+
     }
 }
